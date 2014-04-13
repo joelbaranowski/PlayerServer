c1cb95c0dfe11df6388e0828f87c162ef05e2e0e
joelbaranowski <joelbaranowski@yahoo.com>
2014-04-13 00:22:17 -0500

    other app
    sends get to other app

--- src/request/TestPost.java
+++ src/request/TestPost.java
@@ -10,7 +10,7 @@ public class TestPost {
 
 
   public String run( HttpServletResponse  resp){
-	  MakePost mp = new MakePost("http://twitter.com/statuses/user_timeline/14437022.rss");
+	  MakePost mp = new MakePost("http://1-dot-utopian-hearth-532.appspot.com/test2");
 	    try {
 	      System.out.println(mp.execute(resp));
 	    } catch (Exception exception) {
